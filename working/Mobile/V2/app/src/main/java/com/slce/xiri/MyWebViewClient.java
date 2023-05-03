ffc2d011c31fac41a7142d9b822f5c46d416b455
Casper Ong <casper10528@gmail.com>
2023-05-03 23:22:39 +0800

    couldn't resolve

---
+++ working/Mobile/V2/app/src/main/java/com/slce/xiri/MyWebViewClient.java
@@ -0,0 +1,25 @@
+package com.slce.xiri;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class MyWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        String hostname;
+
+        // YOUR HOSTNAME
+        hostname = "rewrite.slce-group4.eu.org";
+
+        Uri uri = Uri.parse(url);
+        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
+            return false;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
--- working/Mobile/V2/xiri/android/app/src/main/kotlin/com/example/xiri/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.xiri
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
