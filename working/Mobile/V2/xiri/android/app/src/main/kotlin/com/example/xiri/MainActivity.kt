de14c360cd24341023d031704524e7b8869b43e5
Casper Ong <casper10528@gmail.com>
2023-05-03 21:11:45 +0800

    updated working files, trying to get webview on mobile working.

---
+++ working/Mobile/V2/xiri/android/app/src/main/kotlin/com/example/xiri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xiri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ working/Mobile/V2/xiri/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('https://rewrite.slce-group4.org/'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
